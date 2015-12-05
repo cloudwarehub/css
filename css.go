8822ed8ab1483a925ff655e5bf144b29800ce3da
guodong <gd@tongjo.com>
2015-12-05 14:08:01 +0000

    format

--- css.go
+++ css.go
@@ -3,20 +3,20 @@ package main
 import (
 	"flag"
 	"fmt"
-	"github.com/gorilla/websocket"
 	"github.com/bitly/go-simplejson"
-	"net/http"
-	"net/url"
+	"github.com/gorilla/websocket"
 	"io/ioutil"
 	"log"
+	"net/http"
+	"net/url"
 )
 
 type Uconn struct {
-	ws   *websocket.Conn
+	ws      *websocket.Conn
 	user_id string
-	token string
-	authed int
-	runch chan int
+	token   string
+	authed  int
+	runch   chan int
 }
 
 var upgrader = websocket.Upgrader{
@@ -25,44 +25,43 @@ var upgrader = websocket.Upgrader{
 	},
 }
 
-
 func auth(uconn *Uconn) {
-    resp, err := http.Get("http://api.cloudwarehub.com/user?token=" + uconn.token)
+	resp, err := http.Get("http://api.cloudwarehub.com/user?token=" + uconn.token)
 	if err != nil {
-	    fmt.Println("api access error")
+		fmt.Println("api access error")
 		return
 	}
-	
+
 	js, err := ioutil.ReadAll(resp.Body)
 	resp.Body.Close()
 	if err != nil {
 		fmt.Println(err)
 	}
-	
+
 	obj, err := simplejson.NewJson(js)
 	if obj.Get("code").MustInt() != 0 {
-	    uconn.ws.Close()
-	    uconn.runch <- 1 // wakeup and exit recv routine
-	    return
+		uconn.ws.Close()
+		uconn.runch <- 1 // wakeup and exit recv routine
+		return
 	}
 	info := obj.Get("data").MustMap()
 	fmt.Println(info)
-	
+
 	uconn.user_id = info["id"].(string)
 	uconn.authed = 1
 	uconn.runch <- 1
 }
 
 func recv(uconn *Uconn) {
-    /*
-    wait until auch success
-    */
-    <-uconn.runch
-    if uconn.authed == 0 { //auth failed, exit recv goroutine
-        return
-    }
-    ws := uconn.ws
-    for {
+	/*
+	   wait until auch success
+	*/
+	<-uconn.runch
+	if uconn.authed == 0 { //auth failed, exit recv goroutine
+		return
+	}
+	ws := uconn.ws
+	for {
 		mt, message, err := ws.ReadMessage()
 		if err != nil {
 			log.Println("read:", err)
@@ -82,30 +81,29 @@ To achieve none-blocking connect:
     1. upgrade to websocket directly and set status to unauthed
     2. goroutine auth and get storage information, set status to authed
     3. goroutine recv
-    
+
     if status is unauthed, recv routine just ignore all messages from client
 */
 func handler(w http.ResponseWriter, r *http.Request) {
-    fmt.Println("new client")
+	fmt.Println("new client")
 	ws, err := upgrader.Upgrade(w, r, nil)
 	if err != nil {
 		fmt.Println(err)
 		return
 	}
-	
 
 	queryForm, err := url.ParseQuery(r.URL.RawQuery)
 	if err != nil {
-	    log.Println("url parse error")
-	    ws.Close()
-	    return
+		log.Println("url parse error")
+		ws.Close()
+		return
 	}
-	
+
 	if len(queryForm["token"]) < 0 {
-	    ws.Close()
-	    return
+		ws.Close()
+		return
 	}
-	
+
 	token := queryForm["token"][0]
 	uconn := &Uconn{ws: ws, token: token, authed: 0, runch: make(chan int)}
 	go auth(uconn)
